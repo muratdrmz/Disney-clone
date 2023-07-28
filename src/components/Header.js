e2c522d316de68ee2edcaffc3258075999f99401
muratdrmz <mrtdrmz@windowslive.com>
2023-07-28 18:05:49 +0200

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,7 @@ function Header() {
           <span>SERIES</span>
         </a>
       </NavMenu>
+      <UserImg src="/images/murat durmaz.jpg"/>
     </Nav>
   );
 }
@@ -51,17 +52,50 @@ const Logo=styled.img`
 
 const NavMenu=styled.div`
  display:flex;
+ flex:1;
+ margin-left:25px;
  a{
   display:flex;
   align-items:center;
   padding:0 12px;
+  align-items:center;
+  cursor:pointer;
   img{
    height:20px;
   }
   span{
    font-size:13px;
-   letter-spacing:1.42px
+   letter-spacing:1.42px;
+   position:relative;
+
+   &:after{
+    content:"";
+    height:2px;
+    background:white;
+    position:absolute;
+    left:0;
+    right:0;
+    bottom:-6px;
+    opacity:0;
+    transform-origin:left center;
+    transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+    transform:scaleX(0);
+
+   }
+  }
+
+  &:hover{
+    span:after{
+      transform:scaleX(1);
+      opacity:1;
+    }
   }
  }
+`
+
+const UserImg=styled.img`
+width:40px;
+height:40px;
+border-radius:50%;
 
 `
